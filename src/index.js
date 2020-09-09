9a2e10c017a75d39da5731c45a9ea3042c1a7a51
Adam <ruffneradam@aol.com>
2020-09-09 18:40:47 -0400

    content is on page, photo, titles, copyright

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+export const BASE_URL = 'https://api.nasa.gov'
+export const API_KEY = 'uZ44Q7fcKVVxiVhEjlPVFgR2z42qz6BeShaolNYz' 
 ReactDOM.render(<App />, document.getElementById("root"));
